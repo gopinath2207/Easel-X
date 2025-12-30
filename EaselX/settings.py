6501558531481587e23fbada7cc92a0d492fcabe
gopinath2207 <gopinathu6@gmail.com>
2025-12-30 16:10:05 +0530

    Configure production security settings

--- EaselX/settings.py
+++ EaselX/settings.py
@@ -140,6 +140,16 @@ STATIC_ROOT = BASE_DIR / "production_static"  # Directory where static files wil
 
 if not DEBUG:
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    
+    # Security settings for production
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    SECURE_BROWSER_XSS_FILTER = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    SECURE_HSTS_SECONDS = 31536000  # 1 year
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
 
 MEDIA_URL = '/media/'
 
