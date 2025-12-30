917d6fcc5d55448ca47aa1dcf018837242251e64
gopinath2207 <gopinathu6@gmail.com>
2025-12-30 17:12:38 +0530

    Force Serve Media Files: Fix broken images in production

--- EaselX/urls.py
+++ EaselX/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +25,5 @@ urlpatterns = [
     path('core/', include('Core.urls')),
     path('artist/', include('artist.urls')),
     path('customer/', include('Customer.urls')),
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+]
